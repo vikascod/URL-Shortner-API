3bb2add5822c17e6a8ce10837d5fa84eba04ca3c
Vikas <vikas1618072@gmail.com>
2023-02-07 17:21:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database import Base, engine, get_db
+import models
+import random
+
+models.Base.metadata.create_all(engine)
+
+app = FastAPI()
+
+@app.post("/shorten")
+async def shorten_url(url: str, db:Session=Depends(get_db)):
+    char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    tiny_url_id = ''.join(random.choices(char, k=6))
+    tiny_url = f"http://localhost:8000/{tiny_url_id}"
+    tiny = models.TinyURL(original_url=url, tiny_url=tiny_url)
+    db.add(tiny)
+    db.commit()
+    return {"tiny_url": tiny_url}
+
+
+@app.get("/{tiny_url_id}")
+async def redirect_url(tiny_url_id: str, db:Session=Depends(get_db)):
+    tiny = db.query(models.TinyURL).filter_by(tiny_url=f"http://localhost:8000/{tiny_url_id}").first()
+    if not tiny:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="URL not found")
+    return {'url':tiny.original_url}
---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+import datetime
+
+
+class TinyURL(Base):
+    __tablename__ = 'urls'
+
+    id = Column(Integer, primary_key=True)
+    original_url = Column(String, unique=True)
+    tiny_url = Column(String, unique=True)
+    created_at = Column(DateTime, default=datetime.datetime.now())
